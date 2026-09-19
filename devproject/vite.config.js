1940e3ecd424e538c84611229a9e012885742726
whathasmylifeb3com3 <whathasmylifeb3com3@gmail.com>
2026-09-19 04:22:52 +0000

    npm run build

--- devproject/vite.config.js
+++ devproject/vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from vite
-import react from @vitejs/plugin-react
-import mdx from @mdx-js/rollup
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    { enforce: pre, ...mdx() },
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-  ],
+  plugins: [react()],
 })
